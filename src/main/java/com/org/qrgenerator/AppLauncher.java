8b11d04c7404c8ba2061028de7e41847552d0bd6
Anudeep Kumar Donthu <anudeepdonthu@gmail.com>
2020-05-09 15:39:32 +0530

    Updated

--- src/main/java/com/org/qrgenerator/AppLauncher.java
+++ src/main/java/com/org/qrgenerator/AppLauncher.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -15,13 +13,13 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "com.org" })
-public class App /*extends SpringBootServletInitializer */ {
+public class AppLauncher /*extends SpringBootServletInitializer */ {
 
 	@Value("server.port")
 	private static String serverPort;
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(App.class);
+		SpringApplication application = new SpringApplication(AppLauncher.class);
 		application.run(args);
 
 	}
